e29a9d7a8fff182a232e2c2f998cfb3e596fc9b1
Jhan Frank <jhnfrankz@gmail.com>
2024-03-27 15:37:28 -0500

    Added errors for length, location and fill ship place

--- README.md
+++ README.md
@@ -1,38 +1,34 @@
-### Create the field
+### Place all ships
 
-Stage 1/6:Create the field
+Stage 2/6:Place all ships
 
 #### Description
 
-Battleship (also called Battleships or Sea Battle) is a two-player strategy game whose history traces back to the First World War. It started off as a pencil and paper game, until Milton Bradley coined the rules and published the game. Fun fact: it was one of the first games to be produced as a computer game in 1979! In this project, we will recreate this timeless classic.
-
-First off, brush up on the [rules of the game](https://en.wikipedia.org/wiki/Battleship_(game)). There are different variations of the Battleship game, but we will stick to the original rules written by Milton Bradley. You have a 10×10 game field and five ships to arrange on that field. The ships can be placed horizontally or vertically but not diagonally across the grid spaces; the ships should not cross or touch each other. The goal is to sink all the ships of the opponent before your opponent does this to you.
-
-Understanding how the ships will be fielded is exactly where we are going to start!
-
-Before you start, let's discuss the conventions of the game:
-
-1. On a 10x10 field, the first row should contain numbers from 1 to 10 indicating the column, and the first column should contain letters from A to J indicating the row.
-2. The symbol `~` denotes the fog of war: the unknown area on the opponent's field and the yet untouched area on your field.
-3. The symbol `O` denotes a cell with your ship, `X` denotes that the ship was hit, and `M` signifies a miss.
+Now that we've figured out the rules of the game let's continue with positioning the ships and drawing them on our game field! The goal of this stage is to place all the ships on the game field according to the rules.
 
 
 #### Objectives
 
-At this stage, you will put your first ship on the game field.
+You have 5 ships:
 
-1. Print an empty game field, according to game conventions.
-2. To place a ship, enter two coordinates, the beginning and the end of the ship. The order of the coordinates (start to end or end to start) does not really matter.
-3. In later stages we will need to know the length of the ship and the positions on the field on which it's parts are placed. Therefore output this information to a player. Here is an example of possible formatting: `Length: 3` and `Parts: F2 F3 F4`
-4. If an error occurs in the input coordinates (coordinates are not on the same line or out of bounds), your program should report it. The message should contain the word `Error`.
+* Aircraft Carrier is 5 cells
+* Battleship is 4 cells
+* Submarine is 3 cells
+* Cruiser is also 3 cells
+* Destroyer is 2 cells.
+
+In this stage, you should arrange them all on the game field.
+
+1. Start placing your ships with the largest one.
+2. For each ship read two coordinates: the beginning and the end of the ship. Again, the order of the coordinates does not matter.
+3. Add new ships to a game field and output it the same way as in the previous stage.
+4. If the user has entered coordinates in such a way that the length of the created ship does not match the expected length, this should be considered an incorrect input. Also, the game rules state that ships cannot be adjacent to each other. For both of these cases report it with a message containing `Error` word.
 
 
 #### Example
 
 The greater-than symbol followed by a space `>` represents the user input. Note that it's not part of the input.
 
-
-#### Example 1
 
 ```
   1 2 3 4 5 6 7 8 9 10
@@ -46,45 +42,96 @@ G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 J ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-Enter the coordinates of the ship:
-> A1 A4
-Length: 4
-Parts: A1 A2 A3 A4
-```
 
-#### Example 2
+Enter the coordinates of the Aircraft Carrier (5 cells):
 
-```
-...
-Enter the coordinates of the ship:
-> F7 F3
-Length: 5
-Parts: F7 F6 F5 F4 F3
-```
+> F3 F7
 
-#### Example 3
+  1 2 3 4 5 6 7 8 9 10
+A ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+B ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+C ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+D ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+F ~ ~ O O O O O ~ ~ ~
+G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+J ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-```
-...
-Enter the coordinates of the ship:
-> G2 E3
-Error!
-```
+Enter the coordinates of the Battleship (4 cells):
 
-#### Example 4
+> A1 D1
 
-```
-...
-Enter the coordinates of the ship:
-> A11 A14
-Error!
-```
+  1 2 3 4 5 6 7 8 9 10
+A O ~ ~ ~ ~ ~ ~ ~ ~ ~
+B O ~ ~ ~ ~ ~ ~ ~ ~ ~
+C O ~ ~ ~ ~ ~ ~ ~ ~ ~
+D O ~ ~ ~ ~ ~ ~ ~ ~ ~
+E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+F ~ ~ O O O O O ~ ~ ~
+G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+J ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 
-#### Example 5
+Enter the coordinates of the Submarine (3 cells):
 
-```
-...
-Enter the coordinates of the ship:
-> L2 M2
-Error!
+> J7 J10
+
+Error! Wrong length of the Submarine! Try again:
+
+> J10 J8
+
+  1 2 3 4 5 6 7 8 9 10
+A O ~ ~ ~ ~ ~ ~ ~ ~ ~
+B O ~ ~ ~ ~ ~ ~ ~ ~ ~
+C O ~ ~ ~ ~ ~ ~ ~ ~ ~
+D O ~ ~ ~ ~ ~ ~ ~ ~ ~
+E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+F ~ ~ O O O O O ~ ~ ~
+G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+J ~ ~ ~ ~ ~ ~ ~ O O O
+
+Enter the coordinates of the Cruiser (3 cells):
+
+> B9 D8
+
+Error! Wrong ship location! Try again:
+
+> B9 D9
+
+  1 2 3 4 5 6 7 8 9 10
+A O ~ ~ ~ ~ ~ ~ ~ ~ ~
+B O ~ ~ ~ ~ ~ ~ ~ O ~
+C O ~ ~ ~ ~ ~ ~ ~ O ~
+D O ~ ~ ~ ~ ~ ~ ~ O ~
+E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+F ~ ~ O O O O O ~ ~ ~
+G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+I ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+J ~ ~ ~ ~ ~ ~ ~ O O O
+
+Enter the coordinates of the Destroyer (2 cells):
+
+> E6 D6
+
+Error! You placed it too close to another one. Try again:
+
+> I2 J2
+
+  1 2 3 4 5 6 7 8 9 10
+A O ~ ~ ~ ~ ~ ~ ~ ~ ~
+B O ~ ~ ~ ~ ~ ~ ~ O ~
+C O ~ ~ ~ ~ ~ ~ ~ O ~
+D O ~ ~ ~ ~ ~ ~ ~ O ~
+E ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+F ~ ~ O O O O O ~ ~ ~
+G ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+H ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+I ~ O ~ ~ ~ ~ ~ ~ ~ ~
+J ~ O ~ ~ ~ ~ ~ O O O
 ```
--- src/main/java/com/jhnfrankz/battleship/Main.java
+++ src/main/java/com/jhnfrankz/battleship/Main.java
@@ -1,35 +1,44 @@
 package com.jhnfrankz.battleship;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
-    private static String[][] gameField = new String[10][10];
-    private static String[] coordinates;
-    private static boolean error = false;
+    private static final String[][] gameField = new String[10][10];
+    private static String[] strCoordinates;
+    private static int[] coord1;
+    private static int[] coord2;
     private static int length;
-    private static String[] parts;
+    private static List<Ship> ships;
 
     public static void main(String[] args) {
         executeProgram();
     }
 
     public static void executeProgram() {
-        fillGameField();
+        startGameField();
+        fillShips();
         showGameField();
-        readCoordinates();
-
-        if (!error) {
-            calculateLength();
-        }
+        readShipsCoordinates();
     }
 
-    public static void fillGameField() {
+    public static void startGameField() {
         for (int i = 0; i < gameField.length; i++) {
             for (int j = 0; j < gameField[i].length; j++) {
                 gameField[i][j] = "~";
             }
         }
+    }
+
+    public static void fillShips() {
+        ships = new ArrayList<>();
+        ships.add(0, new Ship("Aircraft Carrier", 5));
+        ships.add(1, new Ship("Battleship", 4));
+        ships.add(2, new Ship("Submarine", 3));
+        ships.add(3, new Ship("Cruiser", 3));
+        ships.add(4, new Ship("Destroyer", 2));
     }
 
     public static void showGameField() {
@@ -45,15 +54,34 @@ public class Main {
         }
     }
 
-    public static void readCoordinates() {
-        System.out.println("Enter the coordinates of the ship:");
-        String[] input = scanner.nextLine().trim().split("\\s+");
+    public static void readShipsCoordinates() {
+        for (Ship ship : ships) {
+            System.out.printf("%nEnter the coordinates of the %s (%d cells):%n%n", ship.getName(), ship.getCells());
 
-        if (input.length != 2 || !isValidAlphabetsRow(input) || !isValidNumbersColumn(input)) {
-            System.out.println("Error!");
-            error = true;
-        } else {
-            coordinates = new String[]{input[0], input[1]};
+            do {
+                String[] input = scanner.nextLine().trim().split("\\s+");
+
+                if (input.length != 2 || !isValidAlphabetsRow(input) || !isValidNumbersColumn(input)) {
+                    System.out.println("Error!\n");
+                } else { // Si está bien el input
+                    strCoordinates = new String[]{input[0], input[1]};
+                    if (!isValidLocation()) { //isn't horizontally or vertically
+                        System.out.println("Error! Wrong ship location! Try again:\n");
+                    } else { // Si está bien en orientación
+                        calculateLength();
+
+                        if (!isValidLength(ship.getCells())) { //isn't correct length
+                            System.out.printf("Error! Wrong length of the %s! Try again:\n\n", ship.getName());
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            } while (true);
+
+            // We have valid coords
+            fillShipPlace();
+            showGameField();
         }
     }
 
@@ -70,7 +98,6 @@ public class Main {
         }
 
         return isValid[0] && isValid[1];
-
     }
 
     public static boolean isValidNumbersColumn(String[] input) {
@@ -92,63 +119,65 @@ public class Main {
     }
 
     public static void calculateLength() {
-        String strCoord1 = coordinates[0]; //A1
-        String strCoord2 = coordinates[1]; //A4
+        String strCoord1X = strCoordinates[0].substring(0, 1);   //A1
+        int coord1Y = Integer.parseInt(strCoordinates[0].substring(1));
+        String strCoord2X = strCoordinates[1].substring(0, 1);   //A4
+        int coord2Y = Integer.parseInt(strCoordinates[1].substring(1));
 
-        String strCoord1X = strCoord1.substring(0, 1);
-        int coord1Y = Integer.parseInt(coordinates[0].substring(1));
-        String strCoord2X = strCoord2.substring(0, 1);
-        int coord2Y = Integer.parseInt(coordinates[1].substring(1));
+        //Coords in numbers for array 1
+        int coord1X = fromAlphabetToNumber(strCoord1X); //A = 0
+        coord1Y = coord1Y - 1;                          //1 = 0
 
-        if ((strCoord1X.equals(strCoord2X) && coord1Y != coord2Y)
-            || (!strCoord1X.equals(strCoord2X) && coord1Y == coord2Y)) {
-            //Coords in numbers for array 1
-            int coord1X = fromAlphabetToNumber(strCoord1X); //A = 0
-            coord1Y = coord1Y - 1;                          //1 = 0
+        //Coords in numbers for array 2
+        int coord2X = fromAlphabetToNumber(strCoord2X); //A = 0
+        coord2Y = coord2Y - 1;
 
-            //Coords in numbers for array 2
-            int coord2X = fromAlphabetToNumber(strCoord2X); //A = 0
-            coord2Y = coord2Y - 1;                          //4 = 3
+        coord1 = new int[]{coord1X, coord1Y};
+        coord2 = new int[]{coord2X, coord2Y};
 
-            int[] coord1 = {coord1X, coord1Y};
-            int[] coord2 = {coord2X, coord2Y};
-
-            //LENGTH
-            if (coord1[0] == coord2[0]) {
-                length = Math.abs(coord1[1] - coord2[1]) + 1;
-            } else {
-                length = Math.abs(coord1[0] - coord2[0]) + 1;
-            }
-
-            System.out.println("Length: " + length);
-
-            //PARTS
-            StringBuilder messageParts = new StringBuilder("Parts:");
-            if (coord1[0] == coord2[0]) {  // Si A es igual
-                if (coord1[1] <= coord2[1]) {
-                    for (int i = coord1[1]; i <= coord2[1]; i++) { //generamos numeros
-                        messageParts.append(String.format(" %s%d", strCoord1X, i + 1));
-                    }
-                } else {
-                    for (int i = coord1[1]; i >= coord2[1]; i--) { //generamos numeros
-                        messageParts.append(String.format(" %s%d", strCoord1X, i + 1));
-                    }
-                }
-            } else {  // si 1 es igual
-                if (coord1[0] <= coord2[0]) {
-                    for (int i = coord1[0]; i <= coord2[0]; i++) {
-                        messageParts.append(String.format(" %s%d", fromNumberToAlphabet(i + 1), coord1Y + 1));  //generamos letras
-                    }
-                } else {
-                    for (int i = coord1[0]; i >= coord2[0]; i--) {
-                        messageParts.append(String.format(" %s%d", fromNumberToAlphabet(i + 1), coord1Y + 1));  //generamos letras
-                    }
-                }
-            }
-
-            System.out.println(messageParts);
+        //LENGTH
+        if (coord1[0] == coord2[0]) { //isSameRow
+            length = Math.abs(coord1[1] - coord2[1]) + 1;
         } else {
-            System.out.println("Error"); //, place must be horizontally or vertically
+            length = Math.abs(coord1[0] - coord2[0]) + 1;
+        }
+    }
+
+    public static boolean isValidLocation() {
+        String strCoord1X = strCoordinates[0].substring(0, 1);   //A1
+        int coord1Y = Integer.parseInt(strCoordinates[0].substring(1));
+        String strCoord2X = strCoordinates[1].substring(0, 1);   //A4
+        int coord2Y = Integer.parseInt(strCoordinates[1].substring(1));
+
+        return (strCoord1X.equals(strCoord2X) && coord1Y != coord2Y)
+                || (!strCoord1X.equals(strCoord2X) && coord1Y == coord2Y);
+    }
+
+    public static boolean isValidLength(int shipCells) {
+        return length == shipCells;
+    }
+
+    public static void fillShipPlace() {
+        if (coord1[0] == coord2[0]) {  // Si F es igual
+            if (coord1[1] <= coord2[1]) {  // 2  -  6
+                for (int i = coord1[1]; i <= coord2[1]; i++) {
+                    gameField[coord1[0]][i] = "O";
+                }
+            } else {
+                for (int i = coord1[1]; i >= coord2[1]; i--) {
+                    gameField[coord1[0]][i] = "O";
+                }
+            }
+        } else {  // si 1 es igual
+            if (coord1[0] <= coord2[0]) {   //  A  -  D   ->   0 - 3
+                for (int i = coord1[0]; i <= coord2[0]; i++) {
+                    gameField[i][coord1[1]] = "O";
+                }
+            } else {
+                for (int i = coord1[0]; i >= coord2[0]; i--) {
+                    gameField[i][coord1[1]] = "O";
+                }
+            }
         }
     }
 
@@ -168,8 +197,98 @@ public class Main {
             default -> -1;
         };
     }
+}
 
-    public static String fromNumberToAlphabet(int number) {
+class Ship {
+    private final String name;
+    private final int cells;
+
+    public Ship(String name, int cells) {
+        this.name = name;
+        this.cells = cells;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCells() {
+        return cells;
+    }
+}
+
+
+
+
+/*public static void readCoordinates() {
+    System.out.println("Enter the coordinates of the ship:");
+    String[] input = scanner.nextLine().trim().split("\\s+");
+
+    if (input.length != 2 || !isValidAlphabetsRow(input) || !isValidNumbersColumn(input)) {
+        System.out.println("Error!");
+        error = true;
+    } else {
+        coordinates = new String[]{input[0], input[1]};
+    }
+}*/
+
+
+
+
+
+//PARTS
+        /*StringBuilder messageParts = new StringBuilder("Parts:");
+        if (coord1[0] == coord2[0]) {  // Si A es igual
+            if (coord1[1] <= coord2[1]) {
+                for (int i = coord1[1]; i <= coord2[1]; i++) { //generamos numeros
+                    messageParts.append(String.format(" %s%d", strCoord1X, i + 1));
+                }
+            } else {
+                for (int i = coord1[1]; i >= coord2[1]; i--) { //generamos numeros
+                    messageParts.append(String.format(" %s%d", strCoord1X, i + 1));
+                }
+            }
+        } else {  // si 1 es igual
+            if (coord1[0] <= coord2[0]) {
+                for (int i = coord1[0]; i <= coord2[0]; i++) {
+                    messageParts.append(String.format(" %s%d", fromNumberToAlphabet(i + 1), coord1Y + 1));  //generamos letras
+                }
+            } else {
+                for (int i = coord1[0]; i >= coord2[0]; i--) {
+                    messageParts.append(String.format(" %s%d", fromNumberToAlphabet(i + 1), coord1Y + 1));  //generamos letras
+                }
+            }
+        }
+
+        System.out.println(messageParts);*/
+
+
+
+
+
+
+
+    /*public static boolean isThereError(String[] input, Ship ship) {
+        if (input.length != 2 || !isValidAlphabetsRow(input) || !isValidNumbersColumn(input)) {
+            System.out.println("Error!\n");
+            return true;
+        } else if(!isValidLocation()) { //isn't horizontally or vertically
+            System.out.println("Error! Wrong ship location! Try again:\n");
+            return true;
+        } else if (!isValidLength(ship.getCells())) { //isn't correct length
+            System.out.println("Error! Wrong length of the Submarine! Try again:\n");
+            return true;
+        }
+
+        return false;
+    }*/
+
+
+
+
+
+
+    /*public static String fromNumberToAlphabet(int number) {
         //Coords for array
         return switch (number) {
             case 1 -> "A";
@@ -184,5 +303,4 @@ public class Main {
             case 10 -> "J";
             default -> "_";
         };
-    }
-}
+    }*/
